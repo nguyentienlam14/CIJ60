4880008e1b728caaab1a3f404891d41c7ca2800f
nguyentienlam14 <72743252+nguyentienlam14@users.noreply.github.com>
2021-04-17 19:22:45 +0700

    Add files via upload

---
+++ buoi 3/bai giang/js/Navbar.js
@@ -0,0 +1,17 @@
+const $template = document.createElement('template');
+$template.innerHTML = `
+    <div class="navbar">
+        <div class="navbar-item"><a href="./index.html">home</a></div>
+        <div class="navbar-item"><a href="./about.html">about</a></div>
+        <div class="navbar-item"><a href="./contact.html">contact</a></div>
+    </div>
+`
+
+export default class Navbar extends HTMLElement {
+    constructor() {
+        super()
+        this.appendChild($template.content.cloneNode(true))
+    }
+}
+
+window.customElements.define('my-navbar', Navbar)
---
+++ buoi 3/bai giang/js/app.js
@@ -0,0 +1,73 @@
+/*
+    class calculator 
+        sum(a,b) => a + b
+        subtract(a,b) => a -b
+        multiply(a,b) => a*b
+*/ 
+
+// class Calculator {
+//     static sum (a, b) {
+//         return a + b
+//     }
+
+//     static subtract(a, b) {
+//         return a - b
+//     }
+//     multiply (a, b) {
+//         return a*b
+//     }
+// }
+
+// console.log(Calculator.sum (1,2)) 
+// console.log(Calculator.subtract(3,4))
+
+
+// class person {
+//     firstname
+//     lastname
+//     _age
+
+//     constructor(firstname, lastname) {
+//         this.firstname = firstname
+//         this.lastname = lastname
+//     }
+//     get fullname () {
+//         return this.firstname + "" + this.lastname
+//     }
+//     setAge(age) {
+//         if (age < 0 || age > 150) {
+//             console.log(fails);
+//         } else {
+//             this._age = a 
+//         }
+//     }
+//     get age() {
+//         return this._age
+//     }
+// }
+// let tienLam = new person ("Tien", "Lam" )
+// tienLam.setAge(22)
+// console.log(tienLam.fullname);
+
+
+import Navbar from"./Navbar.js"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
